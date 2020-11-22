5d2db3abf6ba7a8db68af122f72764dd3bc197c6
Murad Sarker <sarker24@gmail.com>
2020-11-23 00:55:45 +0100

    Add the star and the average star and the authentication

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'plays#index'
+  devise_for :users
+  resources :plays do
+    resources :reviews
+  end
+end
---
+++ test/controllers/plays_controller_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class PlaysControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get plays_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get plays_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get plays_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get plays_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get plays_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get plays_destroy_url
+    assert_response :success
+  end
+
+end
